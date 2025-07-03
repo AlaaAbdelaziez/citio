1ebd8b376eb10fbc9e1b1a6741e7476daae18587
Alaa Hassanien <108894722+AlaaAbdelaziez@users.noreply.github.com>
2025-07-03 06:05:40 +0300

    Delete lib/core directory

--- lib/core/utils/assets_image.dart
+++
@@ -1,6 +0,0 @@
-class MyAssetsImage {
-  static const String baseimagepass = "assets/images/";
-  static const String start = "${baseimagepass}SmartCity.jpg";
-  static const String reject = "${baseimagepass}remove.png";
-  static const String logo = "${baseimagepass}logo1.png";
-}
